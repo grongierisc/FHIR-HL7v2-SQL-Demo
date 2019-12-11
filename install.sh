8588ceea1e42f4130fe284f2eedc6e03eaf7e3ae
Administrator <melissali999@hotmail.com>
2019-12-12 00:25:34 +0100

    Update install.sh to create a new application /csp/demo/rest in IRIS

--- install.sh
+++ install.sh
@@ -46,6 +46,20 @@ zw \$classmethod("Ens.Director", "SetAutoStart", "FHIRHL7V2DEMOPKG.FoundationPro
 zn "%SYS"
 Set oCSPApp = ##class(Security.Applications).%OpenId("/csp/healthshare/fhirhl7v2demo/fhir/stu3", , .tSC)
 Set oCSPApp.DispatchClass="FHIRDemo.vSTU3.REST.Handler"
+Set oCSPApp.AutheEnabled = 64
 Set tSC = oCSPApp.%Save()
+
+zn "%SYS"
+set props2("NameSpace") = "$NameSpace"
+set props2("DispatchClass") = "FHIRDemo.REST.Dispatch"
+set props2("CookiePath") = "/csp/demo/rest/"
+set props2("Description") = "Demo REST API"
+set tSC = ##class(Security.Applications).Create("/csp/demo/rest", .props2)
+zw tSCs
+
+w "change application setting"
+set oCSPApp = ##class(Security.Applications).%OpenId("/csp/demo/rest", , .tSC)
+set oCSPApp.AutheEnabled = 32
+do oCSPApp.%Save()
 halt
 EOF
