70695623efaa292e017ef450798d4521ac425fca
Guillaume Rongier <guillaume.rongier@intersystems.com>
2020-04-16 19:13:38 +0200

    Update to fhir r4

--- install.sh
+++ install.sh
@@ -10,49 +10,51 @@ instanceName=$1
 password=password
 
 ClassImportDir=$DIR/src
-NameSpace="FHIRHL7V2DEMO"
+NameSpace=""FHIRHL7V2DEMO""
 
 irissession $instanceName -U USER <<EOF 
 sys
 sys
 zn "HSLIB"
-do ##class(HS.HC.Util.Installer.FHIR).Install()
-$NameSpace
-N
-Y
+Set appKey = "/fhirhl7v2demo/fhir/r4"
+Set strategyClass = "HS.FHIRServer.Storage.Json.InteractionsStrategy"
+Set metadataConfigKey = "HL7v40"
 
-
-Y
-N
-N
-N
-Y
+//Install a Foundation namespace and change to it
+Do ##class(HS.HC.Util.Installer).InstallFoundation("$NameSpace")
 zn "$NameSpace"
+
+// Install elements that are required for a FHIR-enabled namespace
+Do ##class(HS.FHIRServer.Installer).InstallNamespace()
+
+// Install an instance of a FHIR Service into the current namespace
+Do ##class(HS.FHIRServer.Installer).InstallInstance(appKey, strategyClass, metadataConfigKey)
 do ##class(Ens.Director).StopProduction()
 do \$system.OBJ.ImportDir("$ClassImportDir","*.cls","cdk",.errors,1)
-zw ##class(HS.FHIR.DTL.Util.API.ExecDefinition).SetCustomDTLPackage("HS.Local.FHIR.DTL")
 
-zw \$SYSTEM.SQL.Execute("update HS_Registry_Service.HTTP set host = 'fhirhl7v2demo'")
-zw ##class(Ens.Config.Credentials).SetCredential("HS_Services","HS_Services","$password",1)
 zw \$classmethod("Ens.Director", "SetAutoStart", "FHIRHL7V2DEMOPKG.FoundationProduction", 0)
 
-zn "%SYS"
-Set oCSPApp = ##class(Security.Applications).%OpenId("/csp/healthshare/fhirhl7v2demo/fhir/stu3", , .tSC)
-Set oCSPApp.DispatchClass="FHIRDemo.vSTU3.REST.Handler"
-Set oCSPApp.AutheEnabled = 64
-Set tSC = oCSPApp.%Save()
+set cspConfig = ##class(HS.Util.RESTCSPConfig).URLIndexOpen(appKey)
+set cspConfig.ServiceConfigName = "HS.FHIRServer.Interop.Service"
+do cspConfig.%Save()
+
 
 zn "%SYS"
 set props2("NameSpace") = "$NameSpace"
 set props2("DispatchClass") = "FHIRDemo.REST.Dispatch"
 set props2("CookiePath") = "/csp/demo/rest/"
 set props2("Description") = "Demo REST API"
+set props2("MatchRoles") = ":%All"
 set tSC = ##class(Security.Applications).Create("/csp/demo/rest", .props2)
 zw tSCs
 
 w "change application setting"
 set oCSPApp = ##class(Security.Applications).%OpenId("/csp/demo/rest", , .tSC)
-set oCSPApp.AutheEnabled = 32
+set oCSPApp.AutheEnabled = 64
+do oCSPApp.%Save()
+
+set oCSPApp = ##class(Security.Applications).%OpenId(appKey, , .tSC)
+set oCSPApp.AutheEnabled = 64
 do oCSPApp.%Save()
 halt
 EOF
